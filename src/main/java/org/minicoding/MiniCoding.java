efc7eec6fabce84e03b0a04eda0adbe5e68efafe
Chris <My@DESKTOP-JBSD6AK>
2019-03-07 22:30:26 +0800

    完成stage2代码

--- src/main/java/org/minicoding/MiniCoding.java
+++ src/main/java/org/minicoding/MiniCoding.java
@@ -20,4 +20,25 @@ public class MiniCoding {
 			flag = true;
 		}
 	}
+	
+	public static void mathGameStage2() {
+		boolean flag = true;
+		String str_i ;
+		for(int i=1;i<=100;i++) {
+			str_i = String.valueOf(i);
+			if((i%3)==0 || str_i.indexOf("3")>-1) {
+				System.out.print("Fizz");
+				flag = false;
+			}
+			if((i%5)==0 || str_i.indexOf("5")>-1) {
+				System.out.print("Buzz");
+				flag = false;
+			}
+			if(flag) {
+				System.out.print(i);
+			}
+			System.out.println();
+			flag = true;
+		}
+	}
 }
---
+++ src/test/java/test/org/minicoding/MiniCodingTest.java
@@ -0,0 +1,17 @@
+package test.org.minicoding;
+
+import org.junit.Test;
+import org.minicoding.MiniCoding;
+
+public class MiniCodingTest {
+
+	@Test
+	public void mathGameTest() {
+		MiniCoding.mathGame();
+	}
+	@Test
+	public void mathGameStage2Test() {
+		MiniCoding.mathGameStage2();
+	}
+
+}
